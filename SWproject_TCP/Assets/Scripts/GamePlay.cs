d6f15bd6e269ab9b9fb462192a3e3d69af0856df
2017250297-choi <97ckdtn@gmail.com>
2023-11-21 16:50:07 +0900

    Feat: battleLogic ready

--- SWproject_TCP/Assets/Scripts/GamePlay.cs
+++ SWproject_TCP/Assets/Scripts/GamePlay.cs
@@ -8,20 +8,20 @@ using System.Linq;
 
 public class GamePlay : MonoBehaviour
 {
-    public GameObject m_serverPlayerPrefab; // 서버 측 플레이어 캐릭터
-    public GameObject m_clientPlayerPrefab; // 클라이언트 측 플레이어 캐릭터
+    public GameObject m_serverPlayerPrefab; // ???? ?? ???????? ??????
+    public GameObject m_clientPlayerPrefab; // ?????????? ?? ???????? ??????
 
-    public GameObject m_CountdownObject; // 카운트다운 표시 이미지 모음
+    public GameObject m_CountdownObject; // ?????????? ???? ?????? ????
 
-    public GameObject m_damageTextPrefab; // 데미지 표시
+    public GameObject m_damageTextPrefab; // ?????? ????
 
-    public GameObject m_finalResultWinPrefab; // 최종 결과 승리
-    public GameObject m_finalResultLosePrefab; // 최종 결과 패배
+    public GameObject m_finalResultWinPrefab; // ???? ???? ????
+    public GameObject m_finalResultLosePrefab; // ???? ???? ????
 
 
     const int PLAYER_NUM = 2;
-    GameObject m_serverPlayer; // 자주 사용하므로 확보
-    GameObject m_clientPlayer; // 자주 사용하므로 확보
+    GameObject m_serverPlayer; // ???? ?????????? ????
+    GameObject m_clientPlayer; // ???? ?????????? ????
 
     GameState m_gameState = GameState.None;
     InputData[] m_inputData = new InputData[PLAYER_NUM];
@@ -29,34 +29,34 @@ public class GamePlay : MonoBehaviour
     string m_serverAddress;
 
     int m_playerId = 0;
-    int[] m_hp = new int[PLAYER_NUM]; // 서로의 체력
+    int[] m_hp = new int[PLAYER_NUM]; // ?????? ????
     Winner m_actionWinner = Winner.None;
     bool m_isGameOver = false;
 
-    // 공격/회피 송수신 대기용
+    // ????/???? ?????? ??????
     float m_timer;
     bool m_isSendAction;
     bool m_isReceiveAction;
 
-    // 카운트다운용
+    // ????????????
     private float startTime;
 
 
-    // 게임 진행 상황
+    // ???? ???? ????
     enum GameState
     {
         None = 0,
-        Ready, // 게임 상대의 로그인 대기
-        Countdown, // 카운트다운 시작
-        Action, // (게임 시작) 공격/회피 선택, 수신 대기
-        EndAction, // 공격/회피 연출
-        Result, // 결과 발표
-        EndGame, // 끝
-        Disconnect, // 오류 (연결 안 됨)
+        Ready, // ???? ?????? ?????? ????
+        Countdown, // ?????????? ????
+        Action, // (???? ????) ????/???? ????, ???? ????
+        EndAction, // ????/???? ????
+        Result, // ???? ????
+        EndGame, // ??
+        Disconnect, // ???? (???? ?? ??)
     }
 
 
-    // Start is called before the first frame update (Initialize용)
+    // Start is called before the first frame update (Initialize??)
     void Start()
     {
         m_serverPlayer = null;
@@ -66,22 +66,22 @@ public class GamePlay : MonoBehaviour
         m_isSendAction = false;
         m_isReceiveAction = false;
 
-        // 초기화
+        // ??????
         for (int i = 0; i < m_inputData.Length; ++i)
         {
             m_inputData[i].attackInfo.actionKind = ActionKind.None;
             m_inputData[i].attackInfo.actionTime = 0.0f;
         }
 
-        // 아직 동작시키지 않음
+        // ???? ?????????? ????
         m_gameState = GameState.None;
 
         for (int i = 0; i < m_hp.Length; ++i)
         {
-            m_hp[i] = 100; // 체력바 우선 100으로 통일
+            m_hp[i] = 100; // ?????? ???? 100???? ????
         }
 
-        // 통신 모듈 작성
+        // ???? ???? ????
         GameObject go = new GameObject("Network");
         if (go != null)
         {
@@ -93,9 +93,9 @@ public class GamePlay : MonoBehaviour
             DontDestroyOnLoad(go);
         }
 
-        // 호스트명 가져오기
+        // ???????? ????????
         string hostname = Dns.GetHostName();
-        // 호스트명에서 IP주소를 가져옴
+        // ???????????? IP?????? ??????
         //IPAddress[] adrList = Dns.GetHostAddresses(hostname);
         IPHostEntry host = Dns.GetHostEntry(hostname);
         //m_serverAddress = adrList[0].ToString();
@@ -107,7 +107,7 @@ public class GamePlay : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Debug.Log(m_gameState); // 나중에 없애기
+        //Debug.Log(m_gameState); // ?????? ??????
 
         switch(m_gameState)
         {
@@ -157,66 +157,66 @@ public class GamePlay : MonoBehaviour
         float px = Screen.width * 0.5f - 100.0f;
         float py = Screen.height * 0.5f;
 
-        // 미접속일 때의 GUI 표시
+        // ???????? ???? GUI ????
         if (m_networkController == null)
         {
-            if (GUI.Button(new Rect(px, py, 200, 30), "대전 상대를 기다립니다."))
+            if (GUI.Button(new Rect(px, py, 200, 30), "???? ?????? ??????????."))
             {
-                m_networkController = new NetworkController(); // 서버
+                m_networkController = new NetworkController(); // ????
                 m_playerId = 0;
                 m_gameState = GameState.Ready;
                 m_isGameOver = false;
 
-                // 호스트 플레이어 생성
+                // ?????? ???????? ????
                 m_serverPlayer = Instantiate(m_serverPlayerPrefab) as GameObject;
                 m_serverPlayer.name = m_serverPlayerPrefab.name;
 
-                GameObject.Find("Title").SetActive(false); // 타이틀 표시 OFF
+                GameObject.Find("Title").SetActive(false); // ?????? ???? OFF
 
-                Debug.Log("서버 생성, 접속");
+                Debug.Log("???? ????, ????");
             }
 
-            // 클라이언트를 선택했을 때 접속할 서버의 주소 입력
+            // ???????????? ???????? ?? ?????? ?????? ???? ????
             Rect labelRect = new Rect(px, py + 80, 200, 30);
             GUIStyle style = new GUIStyle();
             style.fontStyle = FontStyle.Bold;
             style.normal.textColor = Color.black;
-            GUI.Label(labelRect, "상대방 IP 주소", style);
+            GUI.Label(labelRect, "?????? IP ????", style);
             m_serverAddress = GUI.TextField(new Rect(px, py + 95, 200, 30), m_serverAddress);
 
-            if (GUI.Button(new Rect(px, py + 40, 200, 30), "대전 상대와 접속합니다"))
+            if (GUI.Button(new Rect(px, py + 40, 200, 30), "???? ?????? ??????????"))
             {
-                m_networkController = new NetworkController(m_serverAddress); // 서버
+                m_networkController = new NetworkController(m_serverAddress); // ????
                 m_playerId = 1;
                 m_gameState = GameState.Ready;
                 m_isGameOver = false;
 
-                // 클라이언트 플레이어 생성
+                // ?????????? ???????? ????
                 m_clientPlayer = Instantiate(m_clientPlayerPrefab) as GameObject;
                 m_clientPlayer.name = m_clientPlayerPrefab.name;
 
                 GameObject.Find("Title").SetActive(false);
 
-                Debug.Log("클라이언트 접속");
+                Debug.Log("?????????? ????");
             }
         }
     }
 
 
-    // 로그인 대기
+    // ?????? ????
     void UpdateReady()
     {
-        // 접속 확인
+        // ???? ????
         if (m_networkController.IsConnected() == false)
         {
-            return; // 접속 상태 아니면 종료
+            return; // ???? ???? ?????? ????
         }
 
-        // 플레이어 캐릭터가 만들어지지 않았으면 생성함
+        // ???????? ???????? ?????????? ???????? ??????
         if (m_serverPlayer == null)
         {
             m_serverPlayer = Instantiate(m_serverPlayerPrefab) as GameObject;
-            // Instantiate as GameObject 설명: https://codingmania.tistory.com/248
+            // Instantiate as GameObject ????: https://codingmania.tistory.com/248
             m_serverPlayer.name = m_serverPlayerPrefab.name;
         }
         if (m_clientPlayer == null)
@@ -225,7 +225,7 @@ public class GamePlay : MonoBehaviour
             m_clientPlayer.name = m_clientPlayerPrefab.name;
         }
 
-        // 모션이 Idle이 될 때까지 대기
+        // ?????? Idle?? ?? ?????? ????
         if (m_serverPlayer.GetComponent<Player>().IsIdleAnimation() == false)
         {
             return;
@@ -235,22 +235,22 @@ public class GamePlay : MonoBehaviour
             return;
         }
 
-        // 대기 통과 후 다음 상태(카운트다운)로.
+        // ???? ???? ?? ???? ????(??????????)??.
         m_gameState = GameState.Countdown;
     }
 
 
-    // 게임 시작 전 카운트다운
+    // ???? ???? ?? ??????????
     void UpdateCountdown()
     {
         GameObject countObj = GameObject.Find("CountdownObject");
         if (countObj == null) 
         {
-            // 연출용 오브젝트가 없다면 생성. 초기 동작임
+            // ?????? ?????????? ?????? ????. ???? ??????
             countObj = Instantiate(m_CountdownObject) as GameObject;
             countObj.name = "CountdownObject";
             Time.timeScale = 0f;
-            StartCoroutine("StartCountdown", countObj); // 카운트다운 코루틴 호출
+            StartCoroutine("StartCountdown", countObj); // ?????????? ?????? ????
             return;
         }
     }
@@ -268,75 +268,75 @@ public class GamePlay : MonoBehaviour
         yield return new WaitForSecondsRealtime(1.5f);
         countObj.SetActive(false);
 
-        Time.timeScale = 1f; // 게임 시작
+        Time.timeScale = 1f; // ???? ????
         m_gameState = GameState.Action;
     }
 
 
-    // 공격/회피 선택(실행)
+    // ????/???? ????(????)
     void UpdateAction()
     {
 
     }
 
-    // 상대방의 공격/회피 통신 대기
+    // ???????? ????/???? ???? ????
     void UpdateWaitAction()
     {
-        // 수신대기
+        // ????????
     }
 
 
-    // 체력바 반영
+    // ?????? ????
     void UpdateHP()
     {
 
     }
 
 
-    // 게임 종료
+    // ???? ????
     void UpdateEndGame()
     {
 
     }
 
 
-    // 게임 종료 시 화면
+    // ???? ???? ?? ????
     void OnGUIEndGame()
     {
 
     }
 
 
-    // 게임 종료 체크
+    // ???? ???? ????
     public bool IsGameOver()
     {
         return m_isGameOver;
     }
 
 
-    // 게임 결과 표시
+    // ???? ???? ????
     void UpdateResult()
     {
 
     }
 
 
-    // 이벤트 발생 시 콜백 함수
+    // ?????? ???? ?? ???? ????
     public void EventCallback(NetEventState state)
     {
         switch (state.type)
         {
-            case NetEventType.Disconnect: // 연결이 끊어진 이벤트가 들어오면
+            case NetEventType.Disconnect: // ?????? ?????? ???????? ????????
                 if (m_gameState < GameState.EndGame && m_isGameOver == false)
                 {
-                    m_gameState = GameState.Disconnect; // 게임 상태를 Diconnect로 변경
+                    m_gameState = GameState.Disconnect; // ???? ?????? Diconnect?? ????
                 }
                 break;
         }
     }
 
 
-    // 연결 끊김 알림
+    // ???? ???? ????
     void NotifyDisconnection()
     {
 
--- SWproject_TCP/Assets/Scripts/Player.cs
+++ SWproject_TCP/Assets/Scripts/Player.cs
@@ -4,22 +4,46 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // public AudioClip m_attackSE; // 공격 시 음향
-    // public AudioClip m_avoidSE; // 회피 시 음향 
-    // 등등... 음향도 여기서 정의
+    // public AudioClip m_attackSE; // ???? ?? ????
+    // public AudioClip m_avoidSE; // ???? ?? ???? 
+    // ????... ?????? ?????? ????
 
-    // 애니메이션 정의
+    // ?????????? ????
     public enum Motion
     {
-        Idle, // 대기 동작
-        Attack, // 공격
-        Avoid, // 회피
-        Hurt, // 데미지 입음
+        Idle, // ???? ????
+        Attack, // ????
+        Avoid, // ????
+        Hurt, // ?????? ????
     };
     Motion m_currentMotion;
     Animation m_anim;
     int m_damage;
-
+    [SerializeField] float m_speed = 4.0f;
+    [SerializeField] float m_jumpForce = 7.5f;
+    [SerializeField] float m_rollForce = 6.0f;
+    [SerializeField] bool m_noBlood = false;
+    [SerializeField] GameObject m_slideDust;
+    [SerializeField] GameObject my_healthObj;
+    private Animator m_animator;
+    private Rigidbody2D m_body2d;
+    private Sensor_HeroKnight m_groundSensor;
+    private Sensor_HeroKnight m_wallSensorR1;
+    private Sensor_HeroKnight m_wallSensorR2;
+    private Sensor_HeroKnight m_wallSensorL1;
+    private Sensor_HeroKnight m_wallSensorL2;
+    private bool m_isWallSliding = false;
+    private bool m_grounded = false;
+    private bool m_rolling = false;
+    private int m_facingDirection = 1;
+    private int m_currentAttack = 0;
+    private float m_timeSinceAttack = 0.0f;
+    private float m_delayToIdle = 0.0f;
+    private float m_rollDuration = 0.1f;
+    private float m_rollCurrentTime;
+    private float globalCoolDown = 0.0f;
+    private float originPos;
+    private float enemyPos;
     private void Awake()
     {
         m_currentMotion = Motion.Idle;
@@ -44,13 +68,13 @@ public class Player : MonoBehaviour
                 if (m_anim.isPlaying == false)
                 {
                     ChangeAnimation(Motion.Idle);
-                    // 대기 모션으로 전환할 때 플레이어 표기를 낸다.
+                    // ???? ???????? ?????? ?? ???????? ?????? ????.
                     GameObject board = GameObject.Find("BoardYou");
                     board.GetComponent<BoardYou>().Run();
                 }
                 break;
             */
-            case Motion.Idle: // 대기 모션
+            case Motion.Idle: // ???? ????
             case Motion.Attack:
             case Motion.Avoid:
             case Motion.Hurt:
@@ -67,10 +91,10 @@ public class Player : MonoBehaviour
 
     public void ChangeAnimationAction(ActionKind action)
     {
-        // 서버 클라이언트에서의 판정만 할 수 있으므로
-        // Winner.serverPlayer면 자신의 승리로 다룬다
+        // ???? ???????????????? ?????? ?? ?? ????????
+        // Winner.serverPlayer?? ?????? ?????? ??????
 
-        // case Winner.ServerPlayer: // 공격이 자신의 승
+        // case Winner.ServerPlayer: // ?????? ?????? ??
         // if (action == ActionKind.Attack) {
         // ChangeAnimationAttack();
         // }
@@ -78,14 +102,14 @@ public class Player : MonoBehaviour
     }
 
 
-    // 애니메이션이 끝나면 true 리턴
+    // ???????????? ?????? true ????
     public bool IsCurrentAnimationEnd()
     {
         return (m_anim.isPlaying == false);
     }
 
 
-    // 대기 애니메이션 중이면 true 리턴
+    // ???? ?????????? ?????? true ????
     public bool IsIdleAnimation()
     {
         return (m_currentMotion == Motion.Idle);
